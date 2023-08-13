58f3137a49c49952fc204542b5bc24ea7f8f339c
JHL71 <neko0717@naver.com>
2023-08-13 23:07:10 +0900

    add todo

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,11 @@ const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
 function paintTodo(newTodo) {
-  
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  todoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
